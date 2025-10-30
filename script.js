af62f4d63875362c763db7011331b2787b93b8ad
mateus690 <mateus.lepre@escola.pr.gov.br>
2025-10-30 07:38:13 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,5 +3,9 @@ const avanca = document.querySelectorAll('.btn-proximo');
 avanca.forEach(button => {
     button.addEventListener('click', function() {
         const atual = documenet.querySelector('.ativo');
+        const proximoPasso = 'passo-' + this.getAtribute('data-proximo');
+
+        atual.classList.remove('ativo');
+        document.getElementById(proximoPasso).classList.add('ativo');
     })
 })
